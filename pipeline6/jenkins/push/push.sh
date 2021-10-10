195a2dc7c1887aff011ceba2f79f2626b6f454c0
phuongvtn1992 <58924342+phuongvtn1992@users.noreply.github.com>
2021-10-10 22:13:35 +0700

    Update push.sh

--- pipeline6/jenkins/push/push.sh
+++ pipeline6/jenkins/push/push.sh
@@ -4,11 +4,11 @@ echo "********************"
 echo "** Pushing image ***"
 echo "********************"
 
-IMAGE="maven-project"
+IMAGE="jenkins_maven"
 
 echo "** Logging in ***"
-docker login -u dockerhubnpc -p $PASS
+docker login -u phuongvo1992 -p $PASS
 echo "*** Tagging image ***"
-docker tag $IMAGE:$BUILD_TAG dockerhubnpc/$IMAGE:$BUILD_TAG
+docker tag $IMAGE:$BUILD_TAG phuongvo1992/$IMAGE:$BUILD_TAG
 echo "*** Pushing image ***"
-docker push dockerhubnpc/$IMAGE:$BUILD_TAG
+docker push phuongvo1992/$IMAGE:$BUILD_TAG
